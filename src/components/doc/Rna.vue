1162b9406f12ade21069394eaa1d715e9c6bb7c3
Alexandre De Pablo <alexandre.depablo@gmail.com>
2020-02-27 17:55:15 +0100

    Add RNA documentation

--- src/components/doc/Rna.vue
+++ src/components/doc/Rna.vue
@@ -1,13 +1,27 @@
 <template>
-  <div></div>
+  <div>
+    <documentation-rna-intro />
+    <documentation-rna-endpoint />
+    <documentation-rna-outro />
+  </div>
 </template>
 
 <script>
+import DocumentationRnaIntro from "@/components/doc/rna/Intro";
+import DocumentationRnaEndpoint from "@/components/doc/rna/Endpoint";
+import DocumentationRnaOutro from "@/components/doc/rna/Outro";
+
 export default {
-  name: "DocumentationRna",
+  name: "DocumentationRnaEndpoint",
 
   metaInfo: {
     title: "Documentation API RNA"
+  },
+
+  components: {
+    "documentation-rna-intro": DocumentationRnaIntro,
+    "documentation-rna-endpoint": DocumentationRnaEndpoint,
+    "documentation-rna-outro": DocumentationRnaOutro
   }
 };
 </script>
